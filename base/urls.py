dad1377528ff40928fe9f4f1c433827361392439
yaseen <yaseen.naqqash@gmail.com>
2024-02-01 12:42:42 +0330

    3 hours and 54 mintues into tutorial added messaging System in rooms edit and delete messages in rooms if you are the owner added recent acitivities and latest messages separated 3 "browse topics" , "feeds" and "recent messages" into three seperate html file to use them later in profiel added profile pages and all user data

--- base/urls.py
+++ base/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     path('create-room/',views.createRoom,name='createroomurl'),
     path('room-update/<str:pk>/',views.updateRoom,name='updateroomurl'),
     path('delete/<str:pk>/',views.deleteRoom,name='deleteurl'),
+    path('delete-message/<str:pk>/',views.deleteMessage,name='deleteMessageurl'),
+    path('edit-message/<str:pk>/',views.editMessage,name='editmessageurl'),
     path('login/',views.loginPage,name='loginurl'),
     path('logout/',views.logOut,name='logouturl'),
     path('register/',views.registerPage,name='registerurl'),
+    path('profile/<str:pk>/',views.profile,name='profileurl'),
 
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .models import Room ,Topic
+from .models import Room ,Topic , Message
 from .forms import RoomForm
 from django.http import HttpResponse
 from django.db.models import Q
@@ -52,19 +52,30 @@ def registerPage(request):
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-
+    recent_messages=Message.objects.filter(Q(room__topic__name__icontains=q))[:5]
     rooms = Room.objects.filter(Q(topic__name__icontains = q) |
                                 Q(description__icontains = q) |
                                 Q(name__icontains = q))
     
     topics = Topic.objects.all()
     rooms_count = rooms.count()
-    context = {'topics':topics,'rooms':rooms,'rooms_count':rooms_count}
+    context = {'topics':topics,'rooms':rooms,'rooms_count':rooms_count,'recent_messages':recent_messages}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST' :
+        newmessage=Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('roomurl',pk=room.id)
+
+    context = {'room':room,'room_messages':room_messages,'participants':participants}
     return render (request,'base/room.html',context)
 
 @login_required(login_url='loginurl')
@@ -83,8 +94,8 @@ def createRoom(request):
 def updateRoom(requst,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    if requst.user != room.host:
-        return HttpResponse("you are not allowed here")
+    #if requst.user != room.host:
+    #    return HttpResponse("you are not allowed here")
     if (requst.method == 'POST'):
         form = RoomForm(requst.POST,instance=room)
         if form.is_valid():
@@ -97,13 +108,41 @@ def updateRoom(requst,pk):
 @login_required(login_url='loginurl')
 def deleteRoom(request,pk):
     room = Room.objects.get(id=pk)
-    if request.user != room.host:
-        return HttpResponse("you are not allowed here")
+    #if request.user != room.host:
+    #    return HttpResponse("you are not allowed here")
     if request.method=='POST':
         room.delete()
         return redirect('homeurl')
     context = {'obj':room.name}
     return render(request,'base/delete.html',context)
 
+@login_required(login_url='loginurl')
+def deleteMessage(request,pk):
+    message=Message.objects.get(id=pk)
+    room=message.room
+    if request.method=='POST':
+        message.delete()
+        return redirect('roomurl',pk=room.id)
+    context = {'obj':message}
+    return render(request,'base/delete.html',context)
 
+@login_required(login_url='loginurl')
+def editMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    room = message.room
+    if request.method=='POST':
+        message.body=request.POST.get('body')
+        message.save()
+        return redirect('roomurl',pk=room.id)
+    context = {'message':message}
+    return render(request,'base/edit_message.html',context)
+
+def profile(request,pk):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.all()
+    user = User.objects.get(id=pk)
+    rooms=Room.objects.filter(Q(topic__name__icontains=q)|Q(description__icontains=q)|Q(name__icontains=q),host=user)
+    recent_messages=Message.objects.filter(user=user)[:5]
+    context = {'user':user,'topics':topics,'rooms':rooms,'recent_messages':recent_messages}
+    return render (request,'base/profile.html',context)
 # Create your views here.
